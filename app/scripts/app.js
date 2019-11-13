cf9513c43e14df037945618c4cf1fa5ffa698e60
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-11-13 15:54:07 -0700

    wrote a loop to go through the shuffled array and display each card

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
-// cards array holds all of our card options
+// cards cards holds all of our card options
 let card = document.getElementsByClassName("card");
 let cards = [...card];
-// loop through the cards array and add an evey listener that will display the card on click
+// loop through the cards cards and add an evey listener that will display the card on click
 for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', displayCard);
 };
@@ -10,5 +10,34 @@ var displayCard = function () { // above we are running the display card functio
     // here we created the displayCard function to toggle a class list on when the click event is activated.
     this.classList.toggle("open");
     this.classList.toggle("show");
-    this.classList.toggle("disabled");
+    this.classList.toggle("disabled"); // this class is saying that when a card is shown, if it's clicked again, nothing will happen. it's 'disabled'.
+};
+
+// use the fisher-yates shuffle so that all cards are shuffled on page load, or restart
+function shuffle(cards) {
+    var currentIndex = cards.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex =- 1;
+
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = cards[temporaryValue];
+    }
+    return cards;
 }
+// this will shuffle the cards, but will not change the position of the cards on the game board
+
+// we need to loop through the shuffled array and display each card deck
+const deck = document.querySelector(".deck");
+function startGame() {
+    var shuffledCards = shuffle(cards);
+    for (var i = 0; i < shuffledCards.length; i++) {
+        [].forEach.call(shuffledCards, function(item) {
+            deck.appendChild(item);
+        });
+    }
+}
+
+window.onload = startGame(); // when the window is reloaded, run the startGame() function;
